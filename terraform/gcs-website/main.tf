6479d236826c6b5dd75d8b5028d5077085b77b8b
Mohamed Rasvi <rasvihostings@gmail.com>
2024-11-12 13:39:09 -0300

    Update main.tf

---
+++ terraform/gcs-website/main.tf
@@ -0,0 +1,23 @@
+# terraform/gcs-website/main.tf
+resource "google_storage_bucket" "website_bucket" {
+  name     = var.bucket_name
+  location = "US"
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+
+resource "google_storage_bucket_object" "index" {
+  name         = "index.html"
+  bucket       = google_storage_bucket.website_bucket.name
+  source       = "path/to/local/index.html"
+  content_type = "text/html"
+}
+
+resource "google_storage_bucket_iam_member" "public_rule" {
+  bucket = google_storage_bucket.website_bucket.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
